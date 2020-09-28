cddb2fbed7487c08213f6e334f3f6f716a7907e3
Matthias Malek <matthiasmalek72@gmail.com>
2020-09-28 09:14:57 -0600

    translations

---
+++ business-owners-de.php
@@ -0,0 +1,242 @@
+<?php include 'inc/header.php'; ?>
+		<section class="pt-lg-70 pt-100 pt_tab_60 about_section" id="about">
+			<div class="container">
+			<div class="container-inner-1390 pt-lg-100 pt-sm-30">
+				<div class="row no-gutters">
+					<div class="col-12 col-lg-7">
+						<h2 class="stroke_text px-5 text-uppercase text-center text-lg-left text-sm-left text-xs-left mb-10 mb-lg-0 wow fadeInLeft">Business Owners</h2>
+						<div class="bg-white responsive_xs_pad px-5 pt-120 pb-100 position-relative z-index-1">
+							<div class="headline">
+							<h3 class="mb-5 underline title wow fadeInUp">
+								Are you a business owner looking 
+								to take the next step in securing 
+								your company's legacy? </h3>
+							<h4 class="mb-3 subtitle wow fadeInUp">
+								We specialize in revealing and strengthening a company's inherent potential by pairing existing SMEs with qualified, talented management for continuity and growth. 
+							</h4>
+							</div>
+							<p class="font__family-open-sans text-content wow fadeInUp pb-xs-60">If you are facing a potential succession issue or dealing with an under-managed company outside of your core activity, we are here to help. We specialize in accelerating SME growth and assisting with perfect-match succession to ensure continuity and maximize potential. Business owners choose NCA because we're in it for the long run: we know what it takes to build and run a business, and we care for your business as much as you do. NCA works with you to find the best talent and transfer your company to a new generation of entrepreneurs, supported by resources, to secure your company's legacy and ensure a sustainable future. </p>
+						</div>
+					</div>
+					<div class="col-12 col-lg-5">
+						<div class="position-relative owner_img wow fadeInDown px-lg-0 px-5">
+							<img src="img/owner.jpg" data-src="img/owner.jpg" alt="owner" class="w-100 lazyload mt-lg-70 mt-sm-0">
+							<div class="owner-text">©LinkedIn Sales Navigator - Unsplash</div>
+						</div>
+						<div class="row">
+							<div class="col-12">
+								<div class="bg-primary question-box question_box_responsive wow fadeInUp pb-50 mx-lg-0 mx-5">
+								<a href="" class="d-block p-5 text-white">
+								<h2 class="text-white mb-1">LOOKING FOR A PERFECT MATCH? </h2>
+								<p class="font__family-open-sans font__size-16 question-box-text">NCA works with you to find the best talent to secure your company's legacy.<br> Please use our contact form and our team will get back to you shortly. </p>
+								<div class="text-right arrow_link">
+									<img src="img/arrow.png" alt="arrow" />
+								</div>
+								</a>
+								</div>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+			</div>
+		</section>
+		<section class="intermediaries" id="intermediaries">
+			<div class="container">
+
+			<div class="container-inner-1170 ml-140 ml-sm-0">
+				<h2 class="stroke_text stroke_text_pos2 text-uppercase text-center text-lg-left text-sm-left text-xs-left ml-xs-0 mb-10 mb-lg-0 px-lg-0 px-5 wow fadeInDown ml-3">Inter <br> mediaries</h2>
+				<div class="row no-gutters">
+					<div class="col-12 col-lg-5">
+								<div class="bg-primary question-box question-box-height question_box_responsive ml-lg-auto ml-5 mt-200 wow fadeInLeft">
+								<a href="" class="d-block p-5 text-white h-100">
+								<h2 class="text-white mb-1">DO YOU HAVE AN OPPORTUNITY YOU WOULD LIKE US TO REVIEW?</h2>
+								<p class="font__family-open-sans font__size-16 question-box-text">Please use our contact form to get in touch and we will respond shortly. </p>
+								<div class="text-right arrow_link">
+									<img src="img/arrow.png" alt="arrow" />
+								</div>
+								</a>
+								</div>
+							
+					</div>
+					<div class="col-12 col-lg-7">
+						
+						<div class="bg-white responsive_xs_pad px-5 pt-100 pb-120 position-relative z-index-1 white_box_1170">
+							<div class="headline">
+							<h3 class="mb-5 underline title wow fadeInUp">
+								Are you an intermediary<br> who assists existing businesses<br> with succession?</h3>
+							<h4 class="mb-3 subtitle wow fadeInUp">
+								We specialize in matching exceptional talents<br> with existing SMEs to enhance growth and strengthen sustainable business legacies.
+							</h4>
+							</div>
+							<p class="font__family-open-sans text-content wow fadeInUp">We are constantly building our network of intermediaries and government officials active in SME succession challenges. We are always open to opportunities that may be a great fit for our partners and we work hard to deliver rapid feedback on potential new deals. Please scroll down to see more information about the types of companies we are looking for. </p>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+			</div>
+		</section>
+
+		<section class="where-invest" id="where-invest">
+				<div class="container pt-150 mb-3">
+					<h2 class="stroke_text stroke_text_noposition text-uppercase text-center text-lg-left text-sm-left text-xs-left pl-lg-0 px-xs-50 pl-sm-30 mb-10 mb-lg-0 wow fadeInUp">Where we invest</h2>
+				</div>
+				<div class="container">
+				<div class="container-inner-1260 m-auto pb-lg-180 pb-100 ">
+					<div class="headline headline_edit">
+						<h3 class="mb-5 underline wow fadeInUp font__size-50 responsive_bottom_text line__height-60 pl-lg-0 pl-sm-30 px-xs-50">
+						We are industry agnostic 
+						but we refer to the following 
+						criteria when looking<br> 
+						at potential companies 
+						<br>to acquire.  </h3>
+					</div>
+					<div class="brk-tabs brk-tabs-simple theme-tab wow fadeInUp pl-lg-0 pl-sm-30 px-xs-50" data-hash="true" data-brk-library="component__tabs">
+						<ul class="brk-tabs-nav font__family-efb font__weight-bold mb-3 justify-content-between align-items-end">
+							<li class="brk-tab shadow-none"><span class="font__size-28 text-uppercase">Main Company Criteria</span></li>
+							<li class="brk-tab shadow-none"><span class="font__size-28 text-uppercase">Sector Characteristics</span></li>
+							<li class="brk-tab shadow-none"><span class="font__size-28 text-uppercase">Potential Sectors</span></li>
+						</ul>
+						<div class="brk-tabs-content shadow-none pt-5 px-5 px-xs-20 invest_tabs">
+							<div class="brk-tab-item text-center text-lg-left mt-4 px-5 mx-4 px-xs-0 mx-xs-0">
+								<div class="row">
+									<div class="col-lg-6">
+										<div class="tabs_wrap">
+											<ul>
+												<li class="text-left font__size-20 font__weight-bold"><i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded mt-2"></i>Located in Europe<br> <span class="d-block font__size-16">(primarily in DACH)</span></li>
+											<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-4">
+												<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+												EBITDA in the range of EUR 2-5 million
+											</li>
+												<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-4">
+												<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+												Enterprise Value in the range of EUR 10-35 million
+											</li>
+												<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-4">
+													<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+													Target valuation of 4x-6x EBITDA
+												</li>
+											</ul>
+										</div> 
+									</div>
+									<div class="col-lg-6">
+										<div class="tabs_wrap">
+											<ul>
+												<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-30">
+													<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+													Recurring revenues
+												</li>
+												<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-30">
+													<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+													Profitable with positive cash flows
+												</li>
+												<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-30">
+													<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+													Ability to achieve full control through purchase
+												</li>
+												<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-30">
+													<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+													Space for operational improvement and/or revenue expansion
+												</li>
+
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="brk-tab-item text-center text-lg-left mt-4 px-5 mx-4 px-xs-0 mx-xs-0">
+								<div class="row">
+									<div class="col-lg-4">
+										<div class="tabs_wrap">
+											<ul>
+												<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-4">
+													<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+													Stable or growing
+												</li>
+												<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-4">
+													<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+													Fragmented
+												</li>
+											</ul>
+										</div>
+																				
+									</div>
+									<div class="col-lg-4">
+										<div class="tabs_wrap">
+											<ul>
+												<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-4">
+													<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+													10%+ EBITDA margins
+												</li>
+												<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-4">
+													<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+													Barriers to entry
+												</li>
+											</ul>
+										</div>	
+									</div>
+									<div class="col-lg-4">
+										<div class="tabs_wrap">
+											<ul>
+												<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-4">
+													<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+													Asset light
+												</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="brk-tab-item text-center text-lg-left mt-4 px-5 mx-4 px-xs-0 mx-xs-0">
+								<div class="row">
+									<div class="col-lg-6">
+										<div class="tabs_wrap">
+											<ul>
+												<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-4">
+													<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+													Online or offline education
+												</li>
+												<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-4">
+													<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+													Light manufacturing
+												</li>
+												<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-4">
+													<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+													Software-as-a-Service (SaaS)
+												</li>
+												<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-4">
+													<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+													Healthcare and Wellness through purchase
+												</li>
+
+											</ul>
+										</div>
+									</div>
+									<div class="col-lg-6">
+										<div class="tabs_wrap">
+											<ul>
+												<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-4">
+													<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+													Fintech
+												</li>
+												<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-4">
+													<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+													Service providers of large manufacturing companies
+												</li>
+												<li class="text-left font__size-20 font__weight-bold font__family-efb d-flex align-items-center mb-4">
+													<i class="fa fa-check mr-4 bg-primary p-2 font__size-14 text-white rounded"></i>
+													BPOs
+												</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				</div>
+		</section>
+	<?php include 'inc/footer.php'; ?>
