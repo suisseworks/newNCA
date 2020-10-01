48446c308433257138839eaf6d6156099f3cbf3c
Tuseef Ramzan <ramzantuseef4@gmail.com>
2020-10-01 18:00:31 +0500

    about us changes

--- about-us.php
+++ about-us.php
@@ -291,9 +291,13 @@
                         <div class="member_content bg-white py-4 d-flex align-items-center justify-content-center px-4">
                         	<div>
 	                        	<h5 class="text-center font__size-22 line__height-30 font__family-efb">Meet<br> Jan Nikolaisen</h5>
-			        			<p class="text-center font__size-16 font__family-open-sans line__height-34">NCA Entrepreneur<br>20 <span class="fas fa-arrow-right member_count_arrow px-3 pt-2 d-inline-flex"></span> 22</p>
+			        			<p class="text-center font__size-16 font__family-open-sans line__height-34">NCA Entrepreneur<br>20 <span class=" member_count_arrow px-3 pt-12 d-inline-flex">
+			        				 <svg id="Group_969" data-name="Group 969" xmlns="http://www.w3.org/2000/svg" width="9.093" height="9.093" viewBox="0 0 9.093 9.093">
+									  <path id="Path_10" data-name="Path 10" d="M4.547,0,3.72.827l3.13,3.13H0V5.137H6.85L3.72,8.267l.827.827L9.093,4.547Z" fill="#e57200"/>
+									</svg>
+			        			</span> 22</p>
 			        			<div class="d-flex align-items-center justify-content-center">
-								<a href="searcher-jan.php" target="_blank" class="font__size-12 font__family-open-sans btn btn-prime btn-md-1 learn_more_btn bg-transparent text-capitalize border-radius-25 btn-no-shadow btn_lang px-3 font__weight-bold" data-brk-library="component__button">Learn more</a>
+								<a href="searcher-jan.php" target="_blank" class="font__size-13 font__family-open-sans btn btn-prime learn_more_btn bg-transparent text-capitalize border-radius-25 btn-no-shadow btn_lang px-3 font__weight-bold" data-brk-library="component__button">Learn more</a>
 							</div>	
 							
 						</div>	
@@ -309,7 +313,11 @@
                         <div class="member_content bg-white py-4 d-flex align-items-center justify-content-center px-4">
                         	<a href="talents.php">
 	                        	<h5 class="text-center font__size-22 line__height-30 font__family-efb">Coming soon</h5>
-			        			<p class="text-center font__size-16 font__family-open-sans line__height-34">3 NCA Entrepreneurs<br>20 <span class="fas fa-arrow-right member_count_arrow px-3 pt-2 d-inline-flex"></span> 21</p>
+			        			<p class="text-center font__size-16 font__family-open-sans line__height-34">3 NCA Entrepreneurs<br>20 <span class=" member_count_arrow px-3 pt-12 d-inline-flex">
+				        			 <svg id="Group_969" data-name="Group 969" xmlns="http://www.w3.org/2000/svg" width="9.093" height="9.093" viewBox="0 0 9.093 9.093">
+									  <path id="Path_10" data-name="Path 10" d="M4.547,0,3.72.827l3.13,3.13H0V5.137H6.85L3.72,8.267l.827.827L9.093,4.547Z" fill="#e57200"/>
+									</svg>
+                                 </span> 21</p>
 			        			<div class="d-flex align-items-center justify-content-center">
 			        		</a>		
 						</div>	
