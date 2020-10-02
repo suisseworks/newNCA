741d09cc72b10f7b6d5b778cc361f881605951f5
Matthias Malek <matthiasmalek72@gmail.com>
2020-10-02 15:12:22 -0600

    added back jquery

--- inc/footer-scripts.php
+++ inc/footer-scripts.php
@@ -1,5 +1,5 @@
 	<script src="js/scripts.min.js"></script>
-	<!-- <script src="js/assets/jquery.min.js"></script>  -->
+	<script src="js/assets/jquery.min.js"></script> 
 	<!-- adding jquery.min.js file create conflict with out code and libraries. because its already included in scripts.min.js By adding it twice generates errors in console -->
 
 	<script src="js/custom.js"></script> 
