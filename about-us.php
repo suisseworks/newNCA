ebce91a3a0e0a50b2214a48f4b79db70e536df3c
Matthias Malek <matthiasmalek72@gmail.com>
2021-01-19 07:46:26 -0600

    removed jonathan from about-us pages

--- about-us.php
+++ about-us.php
@@ -601,23 +601,7 @@
 						    </div>	
                         </div>	
 					</div>
-					<div class="col-lg-4 col-lg-4 member_card pl-5 pr-5 mb-10 mt-10 mt-lg-0">
-                        <div class="img_section dummy w-100">
-                        	<img class="w-100" src="img/dummy_img.png">
-                        </div>	
-                        <div class="member_content bg-white py-4 d-flex align-items-center justify-content-center px-4">
-                        	<div>
-	                        	<h5 class="text-center font__size-22 line__height-30 font__family-efb">Meet<br>Jonathan Biron</h5>
-			        			<p class="text-center font__size-16 font__family-open-sans line__height-28 mt-2 member_status">
-			        				Analytics Intern<br>Search Operations Support
-
-			        			</p>
-			        			<div class="d-flex align-items-center justify-content-center mt-3">
-									<a href="https://www.linkedin.com/in/jonathan-biron-0720a517a/" target="_blank" class="social_icon d-inline-flex align-items-center justify-content-center border-radius-5 p-2"><i class="fab fa-linkedin-in"></i></a>
-							    </div>							
-						    </div>	
-                        </div>	
-					</div>
+					 
 				</div>	
 				<div class="row no-gutters new_row mt-5">
 					<div class="col-lg-12 pl-5 pr-5">
