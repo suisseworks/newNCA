0e109ccb91bbc382af1d7646c00697684f74b443
Fazeelat Suleman <fazilat.990@gmail.com>
2021-01-25 17:13:11 +0500

    resolve sliding issue

---
+++ q-a-session-2.php
@@ -0,0 +1,77 @@
+<?php include 'inc/header-scripts.php'; ?>	
+<section class="searcher_sectain searcher_bg">
+	<div class="row no-gutters">
+		<div class="col-lg-6">
+			<div class="searcher_jan_img">
+				<img src="img/Jan.png">
+			</div>
+		</div>
+		<div class="col-lg-6 pr-0">
+				<div class="searcher_content ml-lg-4 mx-lg-0 mx-3">
+					<div class="align-self-lg-stretch">
+						<div class="brk-header__item h-100 d-flex align-items-center">
+							<a href="https://www.linkedin.com/company/30146432/admin/" target="_blank" class="social_icon searcher_linkdin mt-100 d-inline-flex align-items-center ml-2 justify-content-center border-radius-5 p-1 font__family-avenir"><i class="fab fa-linkedin-in font__size-18"></i></a>
+						</div>
+					</div>		
+					<div class="detail_searcher mt-2">
+						<h2 class="font__family-efb text-uppercase line__height-90">entrepreneurship <br> through acquisition</h2>
+						<h4 class="font__size-38 mt-4 subtitle">Free Question & Answer Session</h4>
+						<p class="font__size-18 line__height-24 mt-30">Are you looking to take the next big step in your career? Join our free question and answer session to find out more about our Entrepreneurship Through Acquistion Program. Sig up below and we will provide you with dates and log-in details.
+                        <span class="mt-20 d-block"> Follow our Linkedin page for regular updates, information and tips on how to apply.</span>
+						</p>
+					</div>
+					<div class="brk-theme-options form_side_bar btn-outside bg-primary" data-brk-library="brk-customizer">
+						<div class="contact_me_btn a_q_contact mt-5">
+					<a href="#" type="button" class="btn text-white font__family-efb bg-primary p-5 modal_btn w-100 text-left border-radius-0 brk-theme-options__panels panel-open">Register here
+							<span class="ml-3">
+								<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="30" height="31" viewBox="0 0 30 31">
+								    <defs>
+								    <clipPath id="clip-path">
+								      <rect width="30" height="31" fill="none"/>
+								    </clipPath>
+								    </defs>
+								    <g id="Forward_arrow" data-name="Forward arrow" clip-path="url(#clip-path)">
+								    <path id="Path_10" data-name="Path 10" d="M15.056,0,12.319,2.738,22.682,13.1H0v3.911H22.682L12.319,27.375l2.738,2.738L30.113,15.056Z" fill="#fff"/>
+								    </g>
+							    </svg>
+							</span>
+						</a>
+					</div>
+					</div>
+						<div class="brk-theme-options form_side_bar bg-primary" data-brk-library="brk-customizer">
+						
+						<div class="zoho_form">
+						<div class="row">
+							<div class="col-lg-12">
+								<div class="brk-theme-options__header get_title">
+									<h2 class="text-white font__family-efb font__size-60 text-uppercase">Get<br> In touch</h2>
+								</div>
+								<script src='https://crm.zoho.com/crm/WebFormServeServlet?rid=bcf7596e0334913c2996469d1120e4994619ebb6dcda4d5add2987d670f92cb1gid9b0e913965d8ad37d80439171058312f696514d883aeaa795ecf8019925b9c38&script=$sYG'></script>
+							</div>
+						</div>
+					</div>
+					</div>
+					
+				</div>
+		</div>
+	</div>
+	<!-- <div class="modal right fade form_side_bar" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel2">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content bg-primary">
+
+				<div class="modal-header position-absolute">
+					<button type="button" class="close function_btn text-white" id="info0" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				</div>
+
+				<div class="modal-body p-5">
+					<div class="brk-theme-options__header get_title">
+						<h2 class="text-white font__family-efb font__size-60 text-uppercase">Get<br> In touch</h2>
+					</div>
+					<script src='https://crm.zoho.com/crm/WebFormServeServlet?rid=bcf7596e0334913c2996469d1120e4994619ebb6dcda4d5add2987d670f92cb1gid9b0e913965d8ad37d80439171058312f696514d883aeaa795ecf8019925b9c38&script=$sYG'></script>
+				</div>
+
+			</div>
+		</div>
+	</div> -->
+</section>
+<?php include 'inc/footer-scripts-searcher.php'; ?>
