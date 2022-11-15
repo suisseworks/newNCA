f8b577f7e04cb9a4416ba7046bb8f5e8b9625e1d
Matthias Malek <matthiasmalek72@gmail.com>
2022-11-15 14:33:52 +0100

    changes

--- david-miller.php
+++ david-miller.php
@@ -166,7 +166,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 						</a>
 					</div>
 					<div class="headline">
-							<h4 class="mb-3 underline font__size-32 line__height-48 font__family-efm wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;">Success is empty if you arrive at the finish line alone. The best reward is to get there surrounded by winners</h4>
+							<h4 class="mb-3 underline font__size-32 line__height-48 font__family-efm wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;">Success is empty if you arrive at the finish line alone. The best reward is to get there surrounded by winners.</h4>
 							<h5 class="font__size-23 line__height-35 font__family-open-sans font-weight-bold mt-3 text-uppercase">Howard Schultz</h5>
 							<p class="font__size-16 line__height-35 mt-2 text-uppercase">CEO of Starbucks</p>
 					</div>
