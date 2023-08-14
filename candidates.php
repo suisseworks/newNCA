746d7204e93f1f767597660d00ce11f9ba734b0d
cristiangraham <interlinknetworkscr@gmail.com>
2023-08-14 08:48:32 -0600

    Wendy2

--- candidates.php
+++ candidates.php
@@ -590,7 +590,7 @@ $_SESSION["lan"] = "English";
 				
 				<div class="col-lg-4 col-sm-6  member_card pl-5 pr-5 mb-10 mt-10 mt-lg-0">
 					<div class="img_section w-100">
-						<img class="w-100" src="img/Candidates1.png">
+						<img class="w-100" src="img/dummy_img.png">
 					</div>
 					<div class="member_content bg-primary py-4 d-flex align-items-center justify-content-center px-4">
 						<div>
