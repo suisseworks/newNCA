1bf004c5016d7eabf6fa1222f74c9d803bf821bd
cristiangraham <interlinknetworkscr@gmail.com>
2023-11-28 10:04:14 -0600

    Varios8

--- david-kirubi.php
+++ david-kirubi.php
@@ -165,7 +165,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 						</a>
 					</div>
 					<div class="headline">
-							<h4 class="mb-3 underline font__size-32 line__height-48 font__family-efm wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;">If I have seen further it is by standing on the shoulders of Giants.</h4>
+							<h4 class="mb-3 underline font__size-32 line__height-48 font__family-efm wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;">If I have seen further, it is by standing on the shoulders of Giants.</h4>
 							<h5 class="font__size-23 line__height-35 font__family-open-sans font-weight-bold mt-3 text-uppercase">Sir Isaac Newton</h5>
 							<p class="font__size-16 line__height-35 mt-2 text-uppercase"></p>
 					</div>
