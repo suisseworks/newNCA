4e6b78e704f3a263c34cec9b0527bdbbe8eba660
cristiangraham <interlinknetworkscr@gmail.com>
2024-01-24 10:11:35 -0600

    FRYDERYK

--- news-resources.php
+++ news-resources.php
@@ -134,6 +134,10 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 						<div class="resource_wrap_links">
 
 						<div class="anchore_div anchore_div py-3 px-3 bg-light position-relative">
+								<a href="docs/SUCCESSFUL-ACQUISITION-FOR-NCA-ENTREPRENEUR-FRYDERYK-RDUŁTOWSKI-FINAL.pdf" target="_blank" title=" " class="font__size-20 font__family-efb">Successful Acquisition for NCA Entrepreneur Fryderyk Rdułtowski<span class="arrow_link_a"></span></a>
+							</div>	
+
+						<div class="anchore_div anchore_div py-3 px-3 bg-light position-relative">
 								<a href="https://novastonecapital.sharepoint.com/:b:/s/NCAResourceLibrary/EY3BfKzl2NhKtCKfl79DCAoBYVNBQj4EXeGCFLOUTas1ng?e=sp1q0a" target="_blank" title=" " class="font__size-20 font__family-efb">NCA Program presentation for Q&A Jan 2024<span class="arrow_link_a"></span></a>
 							</div>	
 
