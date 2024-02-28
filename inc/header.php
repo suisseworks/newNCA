f5b05f11256954efa9c665a78d5d199bcb32c210
Matthias Malek <matthiasmalek72@gmail.com>
2024-02-28 14:11:20 -0400

    change

--- inc/header.php
+++ inc/header.php
@@ -85,7 +85,7 @@
 
 						<li class="dropdown d-li">
 							
-							<a href="http://insights.novastone-ca.com/">Insights
+							<a href="">Insights
 								<i class="fa fa-caret-down pl-1"></i>                  
 							</a>
 
