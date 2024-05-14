08b2ac03351951169d52e558e53ac3394a3ef3a0
cristiangraham <interlinknetworkscr@gmail.com>
2024-05-14 08:19:10 -0600

    david

--- entrepreneurs.php
+++ entrepreneurs.php
@@ -236,8 +236,13 @@ $_SESSION["lan"] = "English";
 					</div>	
                     </div>	
 				</div>
- 
+   
 
+
+
+
+
+<!--
 				<div class="col-lg-3 member_card mt-10 mb-10 mt-lg-0 pl-5 pr-5">
                     <div class="img_section w-100">
                     	<img class="w-100" src="img/david-widmer-about.png">
@@ -253,6 +258,12 @@ $_SESSION["lan"] = "English";
 					</div>	
                     </div>	
 				</div>
+-->
+
+
+
+
+
 
 				<div class="col-lg-3 member_card mt-10 mb-10 mt-lg-0 pl-5 pr-5">
                     <div class="img_section w-100">
