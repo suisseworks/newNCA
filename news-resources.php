335c9f4da6c9470265ffa09212ccc4aeed5b2b7e
Matthias Malek <matthiasmalek72@gmail.com>
2024-07-09 17:06:41 +0300

    doc lisa

--- news-resources.php
+++ news-resources.php
@@ -127,7 +127,9 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 						<h2 class="stroke_text text-uppercase mb-60 wow fadeInLeft">Nca <br> News</h2>
 						<div class="resource_wrap_links">
 
-
+						<div class="anchore_div anchore_div py-3 px-3 bg-light position-relative">
+								<a href="docs/SUCCESSFUL_ACQUISITION_FOR_NCA_ENTREPRENEUR_LISA_PIERCEY.pdf" target="_blank" title=" " class="font__size-20 font__family-efb">Successful Acquisition for NCA Entrepreneur Lisa Piercey<span class="arrow_link_a"></span></a>
+							</div>
 						
 
 						<div class="anchore_div anchore_div py-3 px-3 bg-light position-relative">
