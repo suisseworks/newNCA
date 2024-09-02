79a5befa5da4c5c699c7d06d899158bf5b9ac2dc
Muhammad Waqas Amjad <dev.mwaqas@gmail.com>
2024-09-02 21:25:56 +0500

    forza-doors updated

---
+++ forza-doors.php
@@ -0,0 +1,231 @@
+<?php include 'inc/header.php'; ?>
+
+<style>
+	
+.forza-heading .stroke_text{
+    font-size: 88px !important;
+    line-height: 105px !important;
+}
+.forza-title.title{
+     font-size: 98px;
+     line-height: 124.26px;
+     font-weight: 700;
+}
+.forza-subtitle.subtitle{
+    font-size: 28px;
+    font-weight: 400;
+   line-height: 35.5px;
+
+}
+.forza-list ul li strong{
+    font-weight: 700;
+}
+.forza-list ul li{
+    font-size: 18px;
+    line-height: 22.82px;
+    margin-bottom: 15px;
+}
+.forza-top-content .headline p{
+    font-size: 16px;
+}
+.visit-btn .social_icon{
+    font-size: 34.22px;
+    font-weight: 700;
+    line-height: 43.39px;
+    background-color: #D6762C !important;
+}
+.partering-sec{
+    background-color: #0E2336;
+    padding: 70px 0px;
+}
+.content-div p{
+    font-size: 28px;
+    font-style: italic;
+    font-weight: 500;
+    line-height: 35.5px;
+    text-align: left;
+    color: #ffffff;
+    width: 100%;
+}
+.span-bar{
+    width: 25px;
+    background-color: #D6762C;
+    height: 104px;
+    margin-right: 15px;
+}
+.prev-next-text{
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 30.43px;
+}
+.forza-log img{
+    width: 266px;
+}
+.mobile-title,.mobile-logo{
+    display: none;
+}
+.main-page .forza-sec .container{
+    background: inherit;
+}
+
+@media only screen and (max-width: 767px){
+    .mobile-title.title{
+        display: block;
+        font-size: 49.67px;
+        line-height: 62.98px;
+        text-align: center;
+    }
+    .desktop-title,.desktop-logo{
+        display: none;
+    }
+    .mobile-logo{
+        display: block;
+        background-color: #ffffff;
+        padding: 15px 30px;
+        position: relative;
+        max-width: 199px;
+        margin: -40px auto 0px;
+        box-shadow: 0px 1px 3px 0px #00000040;
+    }
+    .forza-heading .stroke_text{
+        font-size: 62px !important;
+        line-height: 67px !important;
+        padding: 0px !important;
+    }
+    .forza-top-content{
+        background-color: #ffffff;
+    }
+    .forza-subtitle.subtitle{
+        font-size: 24px !important;
+    }
+    .partering-sec .content-div.pl-80{
+        padding-left: 0px !important;
+        flex-wrap: wrap;
+    }
+    .m-hidden{
+        display: none;
+    }
+    .span-bar{
+        width: 15.67px;
+        height: 56px;
+    }
+    .content-div p{
+        padding-left: 18px;
+        margin-top: -50px;
+    }
+    .m-company{
+        display: none;
+    }
+    .prev-next-text{
+        font-size: 20px;
+    }
+    .col-4.mbw-70{
+        flex: 0 0 70%;
+        max-width: 70%;
+    }
+    .col-4.mbw-15{
+        flex: 0 0 15%;
+        max-width: 15%;
+    }
+    .underline.m-underline{
+        text-decoration: auto;
+    }
+}
+</style>
+
+<section class="pt-lg-70 pt-70 forza-sec">
+	<div class="container-fluid px-0">
+		<div class="row no-gutters">
+			<div class="col-lg-6 bg-white">
+				<h3 class="mb-3 forza-title mobile-title title text-uppercase wow fadeInUp">
+							Forza Doors
+				</h3>
+				<div class="forza-img w-100">
+					<img src="img/forza-doors.png">
+				</div>	
+				<div class="forza-log mobile-logo text-center">
+					<a href="https://www.forza-doors.com/" target="_blank"><img src="img/forza-logo.png"></a>
+				</div>
+				<div class="forza-heading">
+					  <h2 class="stroke_text px-5 text-uppercase text-center mb-100 wow fadeInLeft">About The Company</h2>
+				</div>
+				<div class="forza-log desktop-logo text-center">
+					<a href="https://www.forza-doors.com/" target="_blank"><img src="img/forza-logo.png"></a>
+				</div>	
+			</div>
+			<div class="col-lg-6">
+				<div class="forza-top-content pl-50">
+					<div class="headline pr-90">
+						<h3 class="mb-3 desktop-title forza-title title text-uppercase wow fadeInUp">
+							Forza Doors
+						</h3>
+						<h4 class="mb-5 forza-subtitle subtitle wow fadeInUp">
+							A leading provider of premium quality veneer & laminate timber products to the UK construction industry, with special focus on doors and frames.
+
+						</h4>
+						<p>
+							Founded in 2010, Forza is the UK's premier high quality timber veneer & laminate doors, panels and door frames supplier for commercial, residential and educational sectors. It designs, manufactures and distributes certified fire-rated timber products in multiple configurations characterised by the highest standard finishes and specification. Forza boasts a modern production facility and an integrated supply chain which allows to shorten the project lead time to a minimum, with the ability to adapt to customers’ demands.  
+						</p>	
+						<div class="forza-list mt-5">
+							<ul>
+								<li>
+									<strong>Headquarters:</strong> Partridge Green, Horsham, West Sussex, UK
+								</li>
+								<li>
+									<a href="http://localhost/newNCA/entrepreneurs.php#ceos"><strong>CEO:</strong> Will Hunnam</a>
+								</li>
+								<li>
+									<strong>In NCA portfolio since:</strong> April 2023
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-12 visit-btn px-0 mt-5">
+						<div class="d-flex align-items-center justify-content-center talent_btn_learn h-100 pb-lg-0 pb-sm-2 pl-lg-50">
+								<a href="https://www.forza-doors.com/" target="_blank" class="social_icon d-inline-flex align-items-center h-100 justify-content-start border-radius-0 p-2 pl-50 text-white bg-primary w-100 text-uppercase">Visit Website</a>
+							</div>
+					</div>
+				</div>	
+			</div>	
+		</div>	
+	</div>	
+</section>	
+
+<section class="partering-sec forza-sec">
+    <div class="container">
+    	<div class="row">
+    		<div class="col-12">
+    			<div class="content-div pl-80 d-flex">
+    				<span class="span-bar"></span> <span class="span-bar"></span>
+    				<p class="underline">
+    					Partnering for profitable growth.<br class="m-hidden"> We focus on transforming SMEs into industry leaders through strategic planning and dedicated support from our experienced team.
+    				</p>
+    			</div>	
+    		</div>	
+    	</div>	
+    </div>	
+</section>	
+
+<section class="next-prev-sec pt-30 pb-30 forza-sec">
+	<div class="container">
+		<div class="row">
+			<div class="col-4 mbw-15">
+				<div class="text-left prev-next-text">
+				   <a href="" class="text-uppercase underline m-underline">< <span class="m-company">Previous Company</span></a>
+				</div>   
+			</div>
+			<div class="col-4 mbw-70">
+				<div class="text-center prev-next-text">
+					<a href="" class="text-uppercase underline">View Portfolio</a>
+				</div>	
+			</div>	
+			<div class="col-4 mbw-15">
+				<div class="text-right prev-next-text">
+				   <a href="" class="text-uppercase underline m-underline"><span class="m-company">Next Company</span> ></a>
+				</div>   
+			</div>	
+		</div>	
+	</div>	
+</section>
+
+<?php include 'inc/footer.php' ?>
