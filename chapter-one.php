7ae6f73f63e9dba171043b3759c83644d4f042bb
cristiangraham <interlinknetworkscr@gmail.com>
2024-09-11 01:56:28 -0600

    01

--- chapter-one.php
+++ chapter-one.php
@@ -1,4 +1,3 @@
-
 <?php include 'inc/header.php'; ?>
 
 <style>
@@ -137,17 +136,15 @@
 <section class="pt-lg-70 pt-70 forza-sec">
 	<div class="container-fluid px-0">
 		<div class="row no-gutters">
-
-
-            <div class="col-lg-6 bg-white">
+			<div class="col-lg-6 bg-white">
 				<h3 class="mb-3 forza-title mobile-title title text-uppercase wow fadeInUp">
-							Forza Doors
+							CHAPTER ONE
 				</h3>
 				<div class="forza-img w-100">
-					<img src="img/forza-doors.png">
+					<img src="img/chapter-one-canada.png">
 				</div>	
 				<div class="forza-log mobile-logo text-center">
-					<a href="https://www.forza-doors.com/" target="_blank"><img src="img/forza-logo.png"></a>
+					<a href="https://www.chapter-1.net/" target="_blank"><img src="img/chapter-one.png"></a>
 				</div>
 				<div class="forza-heading">
 					  <h2 class="stroke_text px-5 text-uppercase text-center mb-100 wow fadeInLeft">About The Company</h2>
@@ -156,57 +153,43 @@
                 <div class="forza-list mt-5 text-center">
 							<ul>
 								<li>
-									<strong>Headquarters:</strong> Partridge Green, Horsham, West Sussex, UK
+									<strong>Headquarters:</strong> Montreal, Canada
 								</li>
 								<li>
-									<a href="entrepreneurs.php#ceos"><strong>CEO:</strong> Will Hunnam</a>
+									<a href="entrepreneurs.php#ceos"><strong>CEO:</strong> David Miller</a>
 								</li>
 								<li>
-									<strong>In NCA portfolio since:</strong> April 2023
+									<strong>In NCA portfolio since:</strong> May 2023
 								</li>
 							</ul>                
                 </div>
                 
 			</div>
-
-
-            
 			<div class="col-lg-6">
 				<div class="forza-top-content pl-50">
 					<div class="headline pr-90">
 						<h3 class="mb-3 desktop-title forza-title title text-uppercase wow fadeInUp">
-							Forza Doors
+							CHAPTER ONE
 						</h3>
                         <div class="forza-log desktop-logo text-left">
-					      <a href="https://www.forza-doors.com/" target="_blank"><img style="width: 217px !important;" src="img/forza-logo.png"></a>
+					      <a href="https://www.chapter-1.net/" target="_blank"><img style="width: 170px !important;" src="img/chapter-one.png"></a>
 				        </div>
                         <br> <br>
 						<h4 class="mb-5 forza-subtitle subtitle wow fadeInUp">
-							A leading provider of premium quality veneer & laminate timber products to the UK construction industry, with special focus on doors and frames.
+                           Chapter One Inc. is a contemporary fashion house designed for the modern woman, offering several brands available in major retailers across the US, Canada, and Europe.
+
 						</h4>
 						<p>
-							Founded in 2010, Forza is the UK's premier high quality timber veneer & laminate doors, panels and door frames supplier for commercial, residential and educational sectors. It designs, manufactures and distributes certified fire-rated timber products in multiple configurations characterised by the highest standard finishes and specification. Forza boasts a modern production facility and an integrated supply chain which allows to shorten the project lead time to a minimum, with the ability to adapt to customers’ demands.<br><br><br><br><br><br><br>
+                           Founded in 1980, Chapter One Inc. is a well-recognized manufacturer and importer of women’s casual and sportswear apparel, including blazers, bottoms, blouses, and dresses. These career-lifestyle focused brands have captured the attention of today's most active and savvy communities across North America and Europe. Chapter One is constantly innovating and expanding its product lines, with a strong emphasis on the latest fashion trends and product quality.<br><br><br><br><br><br><br>
 						</p>	
 					</div>
 					<div class="col-12 visit-btn px-0 mt-5">
 						<div class="d-flex align-items-center justify-content-center talent_btn_learn h-100 pb-lg-0 pb-sm-2 pl-lg-50">
-								<a href="https://www.forza-doors.com/" target="_blank" class="social_icon d-inline-flex align-items-center h-100 justify-content-start border-radius-0 p-2 pl-50 text-white bg-primary w-100 text-uppercase">Visit Website</a>
+								<a href="https://www.chapter-1.net/" target="_blank" class="social_icon d-inline-flex align-items-center h-100 justify-content-start border-radius-0 p-2 pl-50 text-white bg-primary w-100 text-uppercase">Visit Website</a>
 							</div>
 					</div>
 				</div>	
-			</div>
-
-
-
-
-
-
-
-
-
-
-
-
+			</div>	
 		</div>	
 	</div>	
 </section>	
@@ -235,7 +218,9 @@
 				</div>   
 			</div>
 			<div class="col-4 mbw-70">
-				
+				<div class="text-center prev-next-text">
+					<a href="" class="text-uppercase underline">View Portfolio</a>
+				</div>	
 			</div>	
 			<div class="col-4 mbw-15">
 				<div class="text-right prev-next-text">
